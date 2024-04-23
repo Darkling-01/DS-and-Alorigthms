bc6515744326e413a7b978791448788765ff3d32
Alejandro Barragan <57509594+Darkling-01@users.noreply.github.com>
2024-04-22 22:36:15 -0700

    Add files via upload

---
+++ chapter_1.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+#include <string>
+
+
+// Question 1
+
+/*
+Write a program to solve the selection problem.
+Let k = N/2. Draw a table showing the running time
+
+NOTES: Here N represents total number of elements in the array.
+k is the position of the element you're trying to find.
+K = N/2 is finding the element in the middle(median) of the list or array.
+
+*/
+
+// l = left, r = right
+int partition(int arr[], int l, int r){
+
+   int x = arr[r];
+   int i = l;
+
+   for(int j = l; l <= r - 1; j++){
+      if(arr[j] < x){
+        std::swap();
+       }
+   }
+
+
+}
+
+
+int quickSelect(int arr[], int l, int r, int k){
+
+
+
+}
+
+
+int main(){
+
+double processing;  // runtime 
+int arr[];
+int N = 0;
+
+
+
+
+   return 0;
+}
+
+
+
+
