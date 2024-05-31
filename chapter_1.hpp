e0bfcebe64bdf1b16ce5cd0083706998896b2348
Alejandro <barragan13al@gmail.com>
2024-05-30 20:40:42 -0700

    seperate other task for chapter_1.cpp

---
+++ chapter_1.hpp
@@ -0,0 +1,23 @@
+#define CHAPTER_1
+#ifndef CHAPTER_1
+
+#include <vector>
+using std::vector;
+
+
+// Question 1.15
+
+template<typename comparable>
+const Object& findMax(const vector<comparable>& comp){
+	int maxIndex = 0;
+	for(int i = 0; i < comparable.size(); ++i){
+	   if(cmp[maxIndex] < cmp[i])
+	      maxIndex = i; 
+	}
+}
+
+
+
+
+
+#endif
